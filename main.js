a7834f767339389edc6fd8c0db5bf9ce76fa27c1
Jofristie <Joris.ruwen@gmail.com>
2026-02-06 14:14:06 +0100

    Opbouw Van Game
    alleen files en folders

---
+++ main.js
@@ -0,0 +1,38 @@
+import kaboom from "./libs/kaboom.mjs"
+
+kaboom({
+    width: 1280,
+    height: 720,
+    letterbox: true
+}) 
+
+
+const scenes = {
+    menu: () => {
+        add([text("test")])
+    },
+    controls: () => {
+
+    },
+    1: () => {
+
+    },
+    2: () => {
+
+    },
+    3: () => {
+
+    },
+    gameover: () => {
+
+    },
+    end: () => {
+
+    }
+}
+
+for (const key in scenes) {
+    scene(key, scenes[key])
+}
+
+go("menu")
